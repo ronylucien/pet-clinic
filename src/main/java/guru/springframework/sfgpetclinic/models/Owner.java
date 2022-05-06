b1e85db7c348487debc153307afee41f9cfc62a8
ronylucien <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>
2022-05-06 12:21:18 -0400

    Adding POJO's. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/models/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgpetclinic.models;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/models/Vet.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgpetclinic.models;
+
+public class Vet extends Person {
+}
